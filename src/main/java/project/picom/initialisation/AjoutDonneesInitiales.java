3ef19bdbf2c80ae7b5f08ab34153d20d58456da0
nicocposs <msnjus@hotmail.fr>
2022-08-26 19:55:50 +0200

    Back fonctionelle pour les annonces

--- src/main/java/project/picom/initialisation/AjoutDonneesInitiales.java
+++ src/main/java/project/picom/initialisation/AjoutDonneesInitiales.java
@@ -1,7 +1,6 @@
 package project.picom.initialisation;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
